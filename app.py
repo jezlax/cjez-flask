944987ffbde143ea3f633f30f428057eb04f844f
Cory Jez <coryjez@Corys-MBP.attlocal.net>
2021-03-22 22:02:35 -0500

    pushing to git

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
 from flask import Flask, render_template, session, request, redirect, url_for
+from flask_mail import Mail, Message
 from forms import ContactForm
+import smtplib
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'my-key'
 
-@app.route("/")
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'cory.jez1@gmail.com'
+app.config['MAIL_PASSWORD'] = ''
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
+mail = Mail(app)
+
+
+@app.route("/", methods=["GET","POST"])
 def index():
     form = ContactForm()
+    if form.validate_on_submit():
+        name = request.form.get("name")
+        email = request.form.get("email")
+        message = request.form.get("message")
+        notes = '''New message from coryjez.com. \n
+                 From: {} \n
+                 Message: {} \n
+                 Return email: {} \n
+                 Email automatically generated from coryjez.com
+                 '''.format(name,message,email)
+        msg = Message('New Message from CoryJez.com', sender = app.config['MAIL_USERNAME'], recipients =[app.config['MAIL_USERNAME']])
+        msg.body = notes
+        mail.send(msg)
+        return "Sent"
     return render_template("home.html",form=form)
 
 @app.route("/talks")
